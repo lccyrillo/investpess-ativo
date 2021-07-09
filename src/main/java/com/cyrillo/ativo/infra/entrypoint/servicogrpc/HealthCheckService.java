48d938417c8171a6871e9a4430268e3d90cfbd18
Luciano Cavallini Cyrillo <lccyrillo@gmail.com>
2021-07-09 19:46:08 -0300

    CYRILLO_FEAT_0001 - Montar Api de Health check

--- src/main/java/com/cyrillo/ativo/infra/entrypoint/servicogrpc/HealthCheckService.java
+++ src/main/java/com/cyrillo/ativo/infra/entrypoint/servicogrpc/HealthCheckService.java
@@ -1,5 +1,9 @@
 package com.cyrillo.ativo.infra.entrypoint.servicogrpc;
 
+import com.cyrillo.ativo.core.dataprovider.AtivoDtoInterface;
+import com.cyrillo.ativo.core.dataprovider.LogInterface;
+import com.cyrillo.ativo.core.usecase.ListarAtivosPorTipo;
+import com.cyrillo.ativo.infra.config.Sessao;
 import io.grpc.Status;
 import io.grpc.StatusException;
 import io.grpc.health.v1.HealthCheckRequest;
@@ -7,6 +11,7 @@ import io.grpc.health.v1.HealthCheckResponse;
 import io.grpc.health.v1.HealthGrpc.HealthImplBase;
 import io.grpc.stub.StreamObserver;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 public class HealthCheckService extends HealthImplBase {
@@ -26,10 +31,20 @@ public class HealthCheckService extends HealthImplBase {
     }
     private HealthCheckResponse.ServingStatus getServingStatus() {
         // TODO(partner): check the health status of the service
-        //
-        HealthCheckResponse.ServingStatus status = HealthCheckResponse.ServingStatus.SERVING;
+        // Bater nos repositórios da aplicação e ver se estão ok.
+        HealthCheckResponse.ServingStatus status;
+        try {
+            Sessao dataProvider = new Sessao();
+            String uniqueKey = String.valueOf(dataProvider.getUniqueKey());
+            LogInterface log = dataProvider.getLoggingInterface();
+            List<AtivoDtoInterface> lista = null;
+            ListarAtivosPorTipo listarAtivosPorTipo = new ListarAtivosPorTipo();
+            lista = listarAtivosPorTipo.executar(dataProvider,1);
+            status = HealthCheckResponse.ServingStatus.SERVING;
+        }
+        catch (Exception e) {
+            status = HealthCheckResponse.ServingStatus.NOT_SERVING;
+        }
         return status;
-
-       // throw new UnsupportedOperationException("Not implemented yet");
     }
 }
